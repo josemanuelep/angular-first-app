36c983d63e1093eb3e01352731a72d4467701c20
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-12 16:10:10 -0500

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -11,9 +11,4 @@ import { Output, EventEmitter } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
   @Output() notify = new EventEmitter();
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }

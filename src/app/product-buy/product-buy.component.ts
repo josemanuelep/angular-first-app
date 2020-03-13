8e7049cca1cd9abe512a2e249ecd7d757288e3e2
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-13 15:48:43 -0500

    2 files modified

--- src/app/product-buy/product-buy.component.ts
+++ src/app/product-buy/product-buy.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-buy',
   templateUrl: './product-buy.component.html',
@@ -10,13 +11,19 @@ export class ProductBuyComponent implements OnInit {
 
   product;
   productId;
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
-    
   });
+  }
+    addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
 }

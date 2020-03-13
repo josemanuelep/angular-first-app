16cf11f98640faf2f882bb2ed9c11f5f12ff21be
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-13 11:53:45 -0500

    4 files modified, 2 added

--- src/app/product-buy/product-buy.component.ts
+++ src/app/product-buy/product-buy.component.ts
@@ -2,20 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: 'app-product-buy',
+  templateUrl: './product-buy.component.html',
+  styleUrls: ['./product-buy.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductBuyComponent implements OnInit {
+
   product;
   productId;
   constructor(private route: ActivatedRoute) { }
-  
+
   ngOnInit() {
-  this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
-    this.productId = +params.get('productId');
+    
   });
-}
+  }
 
 }

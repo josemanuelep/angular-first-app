bce73044ff63a7bcb78a0f1d7cd60dafd581f29e
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-13 11:56:34 -0500

    1 file modified, 1 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,17 +3,20 @@ export const products = [
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens',
+    iva:15,
     image:'https://elchapuzasinformatico.com/wp-content/uploads/2018/10/pixel-3-xl-google-4.jpg'
   },
   {
     name: 'Phone Mini',
     price: 699,
+    iva:12,
     description: 'A great phone with one of the best cameras',
     image:'https://images-na.ssl-images-amazon.com/images/I/414WXtDphZL.jpg'
   },
   {
     name: 'Phone Standard',
     price: 299,
+    iva:8.5,
     description: '',
     image:'https://www.wholesaleplusltd.com/thumbnail.asp?file=assets/images/wp-a2.jpg&maxx=450&maxy=0'
   }

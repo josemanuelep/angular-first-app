4b921a81e19f0ce9b19cdf28e74aa55f50fe591f
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-15 18:58:51 -0500

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -3,15 +3,21 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CartService {
   items = [];
+  lot = [];
 
   addToCart(product) {
-    this.items.push(product);
+    if(!this.items.find(product)){
+      this.items.push(product);
+      this.lot[this.items.indexOf(product)] +=1;
+    }else{
+      this.lot[this.items.indexOf(product)] +=1;
+    }
   }
 
   getItems() {
     return this.items;
   }
-
+  
   clearCart() {
     this.items = [];
     return this.items;

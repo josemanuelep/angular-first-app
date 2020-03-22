e4f8090a9a4b2f523467727a618af3cbb3cdb9dc
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-22 18:01:44 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [

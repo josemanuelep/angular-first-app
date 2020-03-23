9f522d4d8a09efa0aabb1ae1a8019c7058b72e66
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-22 19:15:45 -0500

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,7 +5,9 @@ import { HttpClient } from '@angular/common/http';
 export class CartService {
   items = [];
   lot = [];
-
+constructor(
+    private http: HttpClient
+  ) {}
   addToCart(product) {
     if(this.items.find(phone => phone.name === product.name)){
      this.lot[this.items.indexOf(product)] +=1;
@@ -27,5 +29,8 @@ export class CartService {
     this.items = [];
     this.lot = [];
     return this.items;
+  }
+   getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 }

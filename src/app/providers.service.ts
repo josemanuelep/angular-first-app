6e9ef6ece480469a225dd29ba9f874e7f71bdd88
josemanuelep <jose.echeverri@upb.edu.co>
2020-03-24 10:13:21 -0500

    1 file modified

--- src/app/providers.service.ts
+++ src/app/providers.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ProvidersService {
-
+  providers=[];
   url ="https://www.datos.gov.co/resource/67wf-gj42.json?$query=select%20distinct%20proveedor";
   
   constructor(  private http: HttpClient) { }
 
   getProviders(){
-      let providers=[];
       this.http.get(this.url).toPromise().then(data => {
-       for (let key in data)
-            providers.push(data[key].proveedor);
+       for (let key in data){
+            this.providers.push(data[key].proveedor);}
     });
-    return providers;
+    return this.providers;
   }
 }
